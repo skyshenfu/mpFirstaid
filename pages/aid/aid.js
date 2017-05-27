f50a6e58c4246c9d63c59b694329d83d350df570
painworkpc <zhangty@all-help.com>
2017-05-27 15:44:04 +0800

    加入首页布局

--- pages/aid/aid.js
+++ pages/aid/aid.js
@@ -2,9 +2,42 @@ var sliderWidth = 110; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
     data: {
-        
+        aidtypes:[]
     },
     onLoad: function () {
-       
+       this.setData({
+         aidtypes:[
+             {
+               "typesrc":"/images/icon_information_highlight.png",
+               "typetext":"name1"
+             },
+             {
+               "typesrc": "/images/icon_information_highlight.png",
+               "typetext": "name2"
+             },
+             {
+               "typesrc": "/images/icon_information_highlight.png",
+               "typetext": "name3"
+             },
+             {
+               "typesrc": "/images/icon_information_highlight.png",
+               "typetext": "name4"
+             }
+             ,
+             {
+               "typesrc": "/images/icon_information_highlight.png",
+               "typetext": "name5"
+             }
+             ,
+             {
+               "typesrc": "/images/icon_information_highlight.png",
+               "typetext": "name6"
+             },
+             {
+               "typesrc": "/images/icon_information_highlight.png",
+               "typetext": "name7"
+             }
+           ]
+       })
     }
 });
