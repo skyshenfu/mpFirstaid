250a458343758351e2b6c53685480f3c940f95fc
painworkpc <zhangty@all-help.com>
2017-06-02 18:19:26 +0800

    完成列表

--- pages/news/news.js
+++ pages/news/news.js
@@ -2,21 +2,40 @@ var sliderWidth = 110; // 需要设置slider的宽度，用于计算中间位置
 var listTemplate = {
   "index": 0,
   "totalpage": 0,
-  "currentpage": 0,
+  "currentpage": 1,
   "items": [],
   "bottomInVisiable": true
 };
 Page({
   data: {
-    list0: Object.assign({}, listTemplate, { "items": [11, 2, 3, 4, 5, 6] }),
+    list0: Object.assign({}, listTemplate),
     list1: Object.assign({},listTemplate),
     list2: Object.assign({}, listTemplate),
     list3: Object.assign({}, listTemplate),
-    tabs: [" 项一", " 项二", " 项三"," 项四"],
+    tabs: ["专业急救", "医学知识", "养生保健","运动健康"],
     activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0,
     locked: false
+  },
+  netRequest: function (classid,goallist){
+    var that=this
+    wx.request({
+      url: 'https://wx.all-help.com/mobile/article/getArticleListByCid.json',
+      method: 'POST',
+      header: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: {
+        'classid': classid
+      },
+      success: function (res) {
+        var name="list"+(classid-1).toString()
+        var datajson={}
+        datajson[name] = Object.assign({}, ...goallist, {
+          items: res.data.data.dataList
+        })
+        that.setData(datajson)
+      }
+    })
   },
   onLoad:function(){
     var that = this;
@@ -29,41 +48,30 @@ Page({
         });
       }
     });
+    wx.showLoading({
+      title: '请稍后'
+    })
+    this.netRequest(1,this.data.list0)
+    this.netRequest(2, this.data.list0)
+    this.netRequest(3, this.data.list0)
+    this.netRequest(4, this.data.list0)
+    wx.hideLoading()
+
   },
-  judgebyindex:function(){
+  judgebyindex:function(goallist){
     if (this.data.locked == true){
       return ;
     }else{
       this.data.locked = true
     }
-    switch (this.data.activeIndex) {
-      case 0:
-        var that = this;
-        setTimeout(function () {
-          if (that.data.list0.items[0] == 1) {
-            that.setData({
-              list0: Object.assign({},that.data.list0,{"items":[11,2,3,4,5,6]}),
-              locked:false
-            })
-          } else {
-            that.setData({
-              list0: Object.assign({}, that.data.list0, { "items": [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] }),
-              locked: false
-            })
-          }
-          wx.stopPullDownRefresh()
-        }, 1000)
-        break;
-      case 1:
-        wx.stopPullDownRefresh()
-        break;
-      case 2:
-        wx.stopPullDownRefresh()
-        break;
-      case 3:
-        wx.stopPullDownRefresh()
-        break;
-    }
+    var that = this;
+    var datajson={}
+    var name = "list"+this.data.activeIndex.toString()
+    var nextitems = goallist.items.concat(res.data.data.dataList)
+    datajson[name] = Object.assign({}, ...goallist, {
+      items: nextitems
+    })
+    that.setData(Object.assign({},datajson,{"locked":false}))
   },
   tabClick: function (e) {
     if(!this.data.locked){
@@ -75,7 +83,7 @@ Page({
   }
   ,
   onPullDownRefresh: function () {
-    this.judgebyindex();
+    this.judgebyindex(this.data.list0);
   },
   scroll: function () {
     console.log("滑动了...")
@@ -98,10 +106,6 @@ Page({
     })
     setTimeout(function () {
       that.data.list0.items.push("new");
-      that.data.list0.items.push("new1");
-      that.data.list0.items.push("new2");
-      that.data.list0.items.push("new3");
-      that.data.list0.items.push("new4");
       that.setData({
         list0: Object.assign({}, that.data.list0, {
           bottomInVisiable: true
--- utils/util.js
+++ utils/util.js
@@ -5,7 +5,6 @@ function formatTime(date) {
   var hour = date.getHours()
   var minute = date.getMinutes()
   var second = date.getSeconds()
-
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
