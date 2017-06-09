8115792ddd3063091d037b2d67b0f87dac1d4a2c
painworkpc <zhangty@all-help.com>
2017-06-09 15:49:36 +0800

    除了数据是假的....

--- pages/aiddetail/aiddetail.js
+++ pages/aiddetail/aiddetail.js
@@ -2,6 +2,46 @@ Page({
     data: {
       "video":"https://wx.all-help.com/static/html/video/yishibuqing_2.mp4",
       "img":"https://wx.all-help.com/static/img/icon/icon_blood.png",
+      "switchs":[],
+      "subfolders": [
+        {
+          "id": 1,
+          "name": "Questions and answers",
+          "type": "qa",
+          "data": [
+            {
+              "id": 1,
+              "question": "什么是流感？",
+              "answer": "流行性感冒，简称流感，是由流感病毒引起的急性呼吸道传染病。流感病毒可分为甲（A）、乙（B）、丙（C）三型。其特点是容易发生变异，其中甲型流感病毒最容易发生变异，可感染人和多种动物，为人类流感的主要病原，常引起大流行和中小流行。乙型变异较少，可感染人类，引起爆发或小流行。丙型较稳定，可感染人类，多为散发病例，目前发现猪也可被感染。最常见的流感起病突然，畏寒、寒战，高热，体温可达39〜40℃，伴头痛、全身肌肉关节酸痛、极度乏力、食欲减退等全身症状，常有咽喉痛、干咳，可有鼻塞、流涕等。如无并发症，多于发病3〜4天后症状好转，但咳嗽、体力恢复常需1〜2周。轻症者如普通感冒，症状轻，2〜3天可恢复。",
+              "image": ""
+            },
+            {
+              "id": 2,
+              "question": "什么是H7N9禽流感？",
+              "answer": "禽流感，是由甲型流感病毒的一种亚型引起的急性传染性疾病。它通常只感染鸟类，少见情况会感染人、猪、马、水貂和海洋哺乳动物。\r\n 可感染人的禽流感病毒亚型为H5N1、H9N2、H7N7、H7N2、H7N3。此次报道的人感染H7N9禽流感病毒，是H7N9和H9N2基因重配的新病毒。2013年以前，中国没有发现人感染H7禽流感病例。",
+              "image": ""
+            },
+            {
+              "id": 3,
+              "question": "人是怎样感染上H7N9禽流感的？",
+              "answer": "对现有病例的流行病学调查提示，活禽市场暴露是发病的危险因素，但人如何感染尚不明确。根据以往经验及本次病例流行病学调查推测，可能由携带H7N9禽流感病毒的禽类及其粪便、羽毛、呼吸道分泌物、血液等，经呼吸道、接触等方式传播给人类。",
+              "image": ""
+            },
+            {
+              "id": 4,
+              "question": "人与人之间会传播H7N9禽流感吗？",
+              "answer": "对现有病例的流行病学调查提示，活禽市场暴露是发病的危险因素，但人如何感染尚不明确。根据以往经验及本次病例流行病学调查推测，可能由携带H7N9禽流感病毒的禽类及其粪便、羽毛、呼吸道分泌物、血液等，经呼吸道、接触等方式传播给人类。",
+              "image": ""
+            },
+            {
+              "id": 5,
+              "question": "人感染H7N9禽流感的潜伏期有多长？",
+              "answer": "根据流感的潜伏期及现有H7N9禽流感病毒感染病例的调查结果，潜伏期一般为7天以内。",
+              "image": ""
+            }
+          ]
+        }
+      ],
       "scenes": [
         {
           "name": "家庭防火：",
@@ -126,6 +166,19 @@ Page({
       ]
     },
     onLoad: function (options) {
-      console.log(options)
+      var that = this
+      console.log(that.data.subfolders)
+      this.setData({
+        switchs: Array(that.data.subfolders[0].data.length).fill(false)
+      })
+    },
+    toggle:function(event){
+      var index = event.currentTarget.id - 1
+      var temparray = this.data.switchs
+      var temp=!temparray[index]
+      temparray[index]=temp
+      this.setData({
+        switchs:temparray
+      })
     }
 });
